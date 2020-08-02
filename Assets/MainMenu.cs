8c33a3e81bcf04fbe82e16f822b21575139a3ec8
andeluvian <kenneth.forsman@gmail.com>
2020-08-02 13:10:56 +0300

    Added function in menu to start levels

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -1,9 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
+    public void PlayLevel()
+    {
+
+        string level = EventSystem.current.currentSelectedGameObject.name;
+        Debug.Log("loading level:" + level);
+        SceneManager.LoadScene(level);
+    }
 public void QuitGame ()
     {
         Debug.Log("Thank you for playing!");
