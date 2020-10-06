5184f37d1689169f379bc63af9bc1029fcfb866b
andeluvian <kenneth.forsman@gmail.com>
2020-10-06 13:58:29 +0300

    Added door controller and switch for opening and closing doors

--- Assets/Scripts/DoorControl.cs
+++ Assets/Scripts/DoorControl.cs
@@ -2,12 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DoorControl : MonoBehaviour
+public class DoorControl : MonoBehaviour, InteractionHandler
 {
+    Animator _doorAnim;
+
+    public void Activate()
+    {
+
+        
+        
+        if(_doorAnim.GetBool("isOpen") == true)
+        {
+            Debug.Log("Door is closing");
+            _doorAnim.SetBool("isOpen",false);
+        }
+        else
+        {
+            Debug.Log("Door is opening");
+            _doorAnim.SetBool("isOpen",true);
+        }
+        
+        
+    }
+    public void Read(){}
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _doorAnim = this.transform.GetComponent<Animator>();
     }
 
     // Update is called once per frame
--- Assets/Scripts/Switch.cs
+++ Assets/Scripts/Switch.cs
@@ -4,17 +4,36 @@ using UnityEngine;
 
 public class Switch : MonoBehaviour , InteractionHandler
 {
+
+
+    [SerializeField]
+    private Color activeColor;
+    [SerializeField]
+    private Color inactiveColor;
+    [SerializeField]
+    private Material material; // currently does not work, TO-DO come back to fix later
+    private bool activated;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        material.SetColor("_EmissionColor", inactiveColor);
     }
 
     // Update is called once per frame
 
     public void Activate()
     {
-        Debug.Log("Hey im activing a cube too");
+        Debug.Log("Switch has been activated");
+        activated = !activated;
+        if (activated)
+        {
+            material.SetColor("_EmissionColor", activeColor);
+        }
+        else
+        {
+            material.SetColor("_EmissionColor", inactiveColor);
+        }
 
     }
     public void Read()
