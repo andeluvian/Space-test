30a83929f9e2182cda2c898917f14e8304a546af
andeluvian <kenneth.forsman@gmail.com>
2020-10-21 02:29:26 +0300

    Added lights script and lightscontroller
    Lights will control the light gameobject and controller will control the parent gameobject that contains all light objects.

---
+++ Assets/Scripts/Lights.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lights : MonoBehaviour, InteractionHandler
+{
+
+private bool LightsOn = false;
+
+public void Read(){}
+public void Activate(){
+
+
+if (LightsOn == false){
+
+    gameObject.SetActive(true);
+    LightsOn = true;
+
+}
+else{
+gameObject.SetActive(false);
+LightsOn = false;
+}
+
+ }
+}
