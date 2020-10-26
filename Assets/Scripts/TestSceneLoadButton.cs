19f2a9e051b5abd3d5c47d18e6e2c4bad2eb62eb
andeluvian <kenneth.forsman@gmail.com>
2020-10-26 14:09:10 +0200

    Experimented with testscene to create minigame to be used through scenemanager loadscene additive

---
+++ Assets/Scripts/TestSceneLoadButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestSceneLoadButton : MonoBehaviour
+{
+      [SerializeField]
+        private string GOToFind;
+
+       public void Button(){
+
+            Debug.Log("BUTTON BETWEEN SCENES WORKS");
+            GameObject.Find(GOToFind);
+            
+
+        }
+
+
+
+}
