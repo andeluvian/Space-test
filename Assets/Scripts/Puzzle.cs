927299d3da098faa8f69dcbc166b6d226b4195a8
andeluvian <kenneth.forsman@gmail.com>
2020-10-28 21:33:11 +0200

    Modified Puzzle script tp update AlarmRoom display objectives

--- Assets/Scripts/Puzzle.cs
+++ Assets/Scripts/Puzzle.cs
@@ -1,14 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Puzzle : MonoBehaviour
 {
-
+[SerializeField] private GameObject currentObjective;
 public bool solved = false;
 
 public bool GetStatus()
 {
+    if(solved == true){
+
+        currentObjective.GetComponent<Toggle>().isOn = true;
+
+    }
+
+
     return solved;
 }
 
