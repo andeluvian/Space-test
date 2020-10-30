af85749aedf6e6c596d00b22b2c721e37e274c39
andeluvian <kenneth.forsman@gmail.com>
2020-10-30 22:21:57 +0200

    Created first puzzle for the game

---
+++ Assets/Scripts/AlarmPuzzle.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlarmPuzzle : MonoBehaviour
+{
+
+
+    [SerializeField] private GameObject[] TurnOnObjects;
+    [SerializeField] private GameObject[] TurnOffObjects;
+    [SerializeField] private GameObject[] DisableObjects;
+    [SerializeField] private GameObject[] EnableObjects;
+
+
+public void TurnOn(){
+
+foreach(GameObject o in TurnOnObjects){
+o.SetActive(true);
+}
+
+}
+public void TurnOff(){
+
+foreach(GameObject o in TurnOffObjects){
+o.SetActive(false);
+}
+}
+public void DisableGameObject(){
+foreach(GameObject o in DisableObjects){
+o.GetComponent<BoxCollider>().enabled = false;
+}
+}
+
+public void EnableGameObject(){
+foreach(GameObject o in EnableObjects){
+
+//o.GetComponent<DoorControl>().enabled = true;
+o.GetComponent<Collider>().enabled = true;
+}
+}
+
+}
--- Assets/Scripts/PuzzleController.cs
+++ Assets/Scripts/PuzzleController.cs
@@ -7,19 +7,25 @@ public class PuzzleController : MonoBehaviour , InteractionHandler
 
     [SerializeField] private GameObject previousPuzzle;
     [SerializeField] private GameObject nextPuzzle;
+
+
     
 
 
 public void Activate(){
 
 
-    if(previousPuzzle.GetComponent<Puzzle>().GetStatus() == true){
+   // if(previousPuzzle.GetComponent<Puzzle>().GetStatus() == true || gameObject != previousPuzzle){
         Debug.Log("This puzzle is ready for you!");
-    }
-    else
-    {
-        Debug.Log("The previous puzzle was not solved!");
-    }
+        gameObject.GetComponent<AlarmPuzzle>().TurnOn();
+        gameObject.GetComponent<AlarmPuzzle>().TurnOff();
+        gameObject.GetComponent<AlarmPuzzle>().DisableGameObject();
+        gameObject.GetComponent<AlarmPuzzle>().EnableGameObject();
+   // }
+  //  else
+   // {
+    //    Debug.Log("The previous puzzle was not solved!");
+   // }
 }
 
 public void Read()
