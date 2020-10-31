d776a2a6f8b131f6cc0b3da35cb04d0c3b3f31d7
andeluvian <kenneth.forsman@gmail.com>
2020-11-01 01:37:56 +0200

    created Password puzzle

--- Assets/Scripts/CheckPassword.cs
+++ Assets/Scripts/CheckPassword.cs
@@ -10,17 +10,58 @@ public Text password;
 public InputField usernameInput;
 public InputField passwordInput;
 
+    [SerializeField] private GameObject[] TurnOnObjects;
+    [SerializeField] private GameObject[] TurnOffObjects;
+    [SerializeField] private GameObject[] DisableObjects;
+    [SerializeField] private GameObject[] EnableObjects;
+
+
 
 public void checkanswer(){
 
-if (username.text.ToUpper() == usernameInput.text.ToUpper()){
-    Debug.Log("Username is correct!");
+if (username.text.ToUpper() == usernameInput.text.ToUpper() && password.text.ToUpper() == passwordInput.text.ToUpper()){
     
+    Debug.Log("Username and Password is correct!");
+    complete();
 
-if (password.text.ToUpper() == passwordInput.text.ToUpper()){
-    Debug.Log("Password is correct!");
-    gameObject.GetComponent<Puzzle>().setSolved();
+}else{
+Debug.Log("Username and Password is incorrect!");
+
 }
+}
+
+public void complete(){
+TurnOn();
+TurnOff();
+DisableGameObject();
+EnableGameObject();
+gameObject.GetComponent<Puzzle>().setSolved();
+}
+
+public void TurnOn(){
+
+foreach(GameObject o in TurnOnObjects){
+o.SetActive(true);
+}
+
+}
+public void TurnOff(){
+
+foreach(GameObject o in TurnOffObjects){
+o.SetActive(false);
+}
+}
+public void DisableGameObject(){
+foreach(GameObject o in DisableObjects){
+o.GetComponent<BoxCollider>().enabled = false;
+}
+}
+
+public void EnableGameObject(){
+foreach(GameObject o in EnableObjects){
+
+//o.GetComponent<DoorControl>().enabled = true;
+o.GetComponent<Collider>().enabled = true;
 }
 }
 }
