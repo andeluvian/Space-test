9b8fac27b6ee6be461831d147da96af218b504b0
andeluvian <kenneth.forsman@gmail.com>
2020-11-01 01:47:34 +0200

    FIX:Passwordpuzzle did not update correctly on completion

--- Assets/Scripts/CheckPassword.cs
+++ Assets/Scripts/CheckPassword.cs
@@ -10,10 +10,7 @@ public Text password;
 public InputField usernameInput;
 public InputField passwordInput;
 
-    [SerializeField] private GameObject[] TurnOnObjects;
-    [SerializeField] private GameObject[] TurnOffObjects;
-    [SerializeField] private GameObject[] DisableObjects;
-    [SerializeField] private GameObject[] EnableObjects;
+[SerializeField] private GameObject parent;
 
 
 
@@ -22,7 +19,7 @@ public void checkanswer(){
 if (username.text.ToUpper() == usernameInput.text.ToUpper() && password.text.ToUpper() == passwordInput.text.ToUpper()){
     
     Debug.Log("Username and Password is correct!");
-    complete();
+    parent.GetComponent<PasswordPuzzle>().complete();
 
 }else{
 Debug.Log("Username and Password is incorrect!");
@@ -30,38 +27,5 @@ Debug.Log("Username and Password is incorrect!");
 }
 }
 
-public void complete(){
-TurnOn();
-TurnOff();
-DisableGameObject();
-EnableGameObject();
-gameObject.GetComponent<Puzzle>().setSolved();
-}
 
-public void TurnOn(){
-
-foreach(GameObject o in TurnOnObjects){
-o.SetActive(true);
-}
-
-}
-public void TurnOff(){
-
-foreach(GameObject o in TurnOffObjects){
-o.SetActive(false);
-}
-}
-public void DisableGameObject(){
-foreach(GameObject o in DisableObjects){
-o.GetComponent<BoxCollider>().enabled = false;
-}
-}
-
-public void EnableGameObject(){
-foreach(GameObject o in EnableObjects){
-
-//o.GetComponent<DoorControl>().enabled = true;
-o.GetComponent<Collider>().enabled = true;
-}
-}
 }
--- Assets/Scripts/PasswordPuzzle.cs
+++ Assets/Scripts/PasswordPuzzle.cs
@@ -7,7 +7,9 @@ public class PasswordPuzzle : MonoBehaviour
 
 
     [SerializeField] private GameObject[] TurnOnObjects;
-
+    [SerializeField] private GameObject[] TurnOffObjects;
+    [SerializeField] private GameObject[] DisableObjects;
+    [SerializeField] private GameObject[] EnableObjects;
     
 public void startPuzzle(){
 
@@ -15,6 +17,41 @@ foreach(GameObject o in TurnOnObjects){
 o.SetActive(true);
 }
 
+}
+
+public void complete(){
+
+TurnOff();
+DisableGameObject();
+EnableGameObject();
+gameObject.GetComponent<Puzzle>().setSolved();
+}
+
+public void TurnOn(){
+
+foreach(GameObject o in TurnOnObjects){
+o.SetActive(true);
+}
+
+}
+public void TurnOff(){
+
+foreach(GameObject o in TurnOffObjects){
+o.SetActive(false);
+}
+}
+public void DisableGameObject(){
+foreach(GameObject o in DisableObjects){
+o.GetComponent<BoxCollider>().enabled = false;
+}
+}
+
+public void EnableGameObject(){
+foreach(GameObject o in EnableObjects){
+
+//o.GetComponent<DoorControl>().enabled = true;
+o.GetComponent<Collider>().enabled = true;
+}
 }
 }
 
