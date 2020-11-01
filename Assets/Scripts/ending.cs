ae3312d2be0757c649608cefe41c2ecc8e65b865
andeluvian <kenneth.forsman@gmail.com>
2020-11-01 17:17:24 +0200

    end mechanic to the game

---
+++ Assets/Scripts/ending.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ending : MonoBehaviour, InteractionHandler
+{
+    [SerializeField] private GameObject Player;
+    // Start is called before the first frame update
+
+ void OnCollisionEnter(Collider other){
+         //other.name should equal the root of your Player object
+         if (other.tag == "Player") {
+             //The scene number to load (in File->Build Settings)
+             SceneManager.LoadScene (0);
+             Application.LoadLevel("menu");
+         }
+     }
+
+
+ void OnTriggerEnter(Collider coll)
+ {
+          //other.name should equal the root of your Player object
+         if (coll.tag == "Player") {
+             //The scene number to load (in File->Build Settings)
+             SceneManager.LoadScene (0);
+             Application.LoadLevel("menu");
+         }
+     }
+
+public void Activate(){
+
+    //  SceneManager.LoadScene (0);
+             Application.LoadLevel("menu");
+}
+
+public void Read(){}
+ }
+
